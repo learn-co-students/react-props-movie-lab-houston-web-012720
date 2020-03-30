959e02dae1d0b885da3b5ac4d0a475b422734e13
somaia-khalil <sumaya_khair@live.com>
2020-03-29 21:15:09 -0500

    Done.

--- src/MovieShowcase.js
+++ src/MovieShowcase.js
@@ -5,8 +5,17 @@ import movieData from './data.js'
 export default class MovieShowcase extends Component {
 
   generateMovieCards = () => {
+    return movieData.map
+    (movie => 
+       < MovieCard title={movie.title}
+        IMDBRating={movie.IMDBRating}
+        genres={movie.genres}
+        poster={movie.poster}/>
+      )
+    }
+  
     // map over your movieData array and return an array of the correct JSX
-  }
+    
 
   render() {
     return (
@@ -16,3 +25,6 @@ export default class MovieShowcase extends Component {
     )
   }
 }
+
+
+
